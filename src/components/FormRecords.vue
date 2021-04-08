e1095a2489eb3b94086fd73057844b653af054d9
Cid Huang <cid@msbt.com.tw>
2021-04-08 17:40:23 +0800

    refactor: FormRecords.vue.

--- src/components/FormRecords.vue
+++ src/components/FormRecords.vue
@@ -1,18 +1,25 @@
 <template lang="pug">
 .list
-	b-row(v-if="records.length > perPage", size='sm')
-		b-col(md='6')
-			b-input-group(size='sm')
-				b-input-group-prepend(is-text) {{ $t('頁次') }}
-				b-form-input(v-model="currentPage")
-		b-col(md='6')
-			b-pagination(size="sm", :total-rows="records.length", v-model="currentPage", :per-page="perPage", align="fill", :limit="paginationLimit", first-number, last-number)
-	b-table(foot-clone, responsive='true', small, striped, sort-by="_id", sort-desc, :items="records", :fields="formHeaderFields", :per-page="perPage", :current-page="currentPage")
-		template(v-slot:cell(_actions)="row")
-			b-button.m-1(v-if="buttonEditRecord", size='sm', :href='editRecordUrl(row.item._id)')
-				| {{ $t('編輯') }}
-			b-button.m-1(v-if="buttonDeleteRecord", size='sm', @click='deleteRecord(row.item._id)')
-				| {{ $t('刪除') }}
+  b-row
+    b-col(md='6')
+      b-input-group(size='sm')
+        b-input-group-prepend
+          b-button.mr-1(v-show="searchMode", @click="searchStop()") {{ $t('結束') }}
+          b-button(@click="searchStart()") {{ $t('搜尋') }}
+        b-form-input(type="text", v-model="searchString")
+  b-row(v-if="records.length > perPage", size='sm')
+    b-col(md='6')
+      b-input-group(size='sm')
+        b-input-group-prepend(is-text) {{ $t('頁次') }}
+        b-form-input(v-model="currentPage")
+    b-col(md='6')
+      b-pagination(size="sm", :total-rows="records.length", v-model="currentPage", :per-page="perPage", align="fill", :limit="paginationLimit", first-number, last-number)
+  b-table(foot-clone, responsive='true', small, striped, sort-by="_id", sort-desc, :items="records", :fields="formHeaderFields", :per-page="perPage", :current-page="currentPage")
+    template(v-slot:cell(_actions)="row")
+      b-button.m-1(v-if="buttonEditRecord", size='sm', :href='editRecordUrl(row.item._id)')
+        | {{ $t('編輯') }}
+      b-button.m-1(v-if="buttonDeleteRecord", size='sm', @click='deleteRecord(row.item._id)')
+        | {{ $t('刪除') }}
 </template>
 <script>
 import { ref, watch, set, toRefs, computed } from "@vue/composition-api";
@@ -58,27 +65,87 @@ export default {
       hiddenFields,
     } = toRefs(props);
 
-    const currentPage = ref(1);
-    if (localStorage.getItem(_id + "currentPage")) {
-      currentPage.value = localStorage.getItem(_id + "currentPage");
+    const allCurrentPage = ref(1);
+    if (localStorage.getItem(_id + "allCurrentPage")) {
+      allCurrentPage.value = localStorage.getItem(_id + "allCurrentPage");
     }
     const formFields = ref([]);
     const formFieldNames = ref([]);
-    const records = ref([]);
+    const allRecords = ref([]);
+
+    const searchCurrentPage = ref(1);
+    if (localStorage.getItem(_id + "searchCurrentPage")) {
+      searchCurrentPage.value = localStorage.getItem(_id + "searchCurrentPage");
+    }
+    const searchRecords = ref([]);
+    const searchMode = ref(false);
+    if (localStorage.getItem(_id + "searchMode")) {
+      searchMode.value = localStorage.getItem(_id + "searchMode") == "true";
+    }
+    const searchString = ref("");
+    if (localStorage.getItem(_id + "searchString")) {
+      searchString.value = localStorage.getItem(_id + "searchString");
+    }
+
+    const currentPage = computed({
+      get: () => {
+        return searchMode.value
+          ? searchCurrentPage.value
+          : allCurrentPage.value;
+      },
+      set: (val) => {
+        if (searchMode.value) {
+          searchCurrentPage.value = val;
+        } else {
+          allCurrentPage.value = val;
+        }
+      },
+    });
+
+    const records = computed(() => {
+      return searchMode.value ? searchRecords.value : allRecords.value;
+    });
+
+    const search = async (str) => {
+      if (str === "") {
+        searchRecords.value = [];
+        return;
+      }
+
+      await gql.formSearch(form.value, formFieldNames.value, str).then(
+        (r) => {
+          searchRecords.value = r;
+        },
+        (e) => {
+          console.error(e);
+        }
+      );
+    };
+
+    const searchStart = () => {
+      searchMode.value = true;
+      localStorage.setItem(_id + "searchMode", searchMode.value);
+      localStorage.setItem(_id + "searchString", searchString.value);
+      search(searchString.value);
+    };
+    const searchStop = () => {
+      searchMode.value = false;
+      localStorage.setItem(_id + "searchMode", searchMode.value);
+    };
 
     const fetchPage = async (page) => {
       if (formFields.value.length <= 0) {
         return;
       }
 
-      let fromId = records.value.length - page * perPage.value;
+      let fromId = allRecords.value.length - page * perPage.value;
       let count = perPage.value;
       if (fromId < 0) {
         count = count + fromId;
         fromId = 0;
       }
       for (let i = count - 1; i >= 0; i--) {
-        if (!records.value[fromId + i][formFields.value[0].name]) {
+        if (!allRecords.value[fromId + i][formFields.value[0].name]) {
           break;
         }
         count--;
@@ -88,10 +155,10 @@ export default {
         .then(
           (r) => {
             for (let i = 0; i < r.length; i++) {
-              if (records.value[fromId + i]._id !== r[i]._id) {
+              if (allRecords.value[fromId + i]._id !== r[i]._id) {
                 continue;
               }
-              set(records.value, fromId + i, r[i]);
+              set(allRecords.value, fromId + i, r[i]);
               continue;
             }
           },
@@ -141,14 +208,18 @@ export default {
 
       await gql.getFormRecordsId(newVal).then(
         (r) => {
-          records.value = r;
+          allRecords.value = r;
         },
         (e) => {
           console.error(e);
         }
       );
 
-      fetchPage(currentPage.value);
+      fetchPage(allCurrentPage.value);
+
+      if (searchMode.value) {
+        await search(searchString.value);
+      }
     };
 
     if (form.value != "") {
@@ -169,9 +240,13 @@ export default {
       }
     );
 
-    watch(currentPage, (newVal, oldVal) => {
-      localStorage.setItem(_id + "currentPage", newVal);
+    watch(allCurrentPage, (newVal, oldVal) => {
+      localStorage.setItem(_id + "allCurrentPage", newVal);
       fetchPage(newVal);
+    });
+
+    watch(searchCurrentPage, (newVal, oldVal) => {
+      localStorage.setItem(_id + "searchCurrentPage", newVal);
     });
 
     const editRecordUrl = (id) => {
@@ -191,13 +266,13 @@ export default {
         return;
       }
 
-      for (const key in records.value) {
-        if (records.value[key]._id === id) {
-          records.value.splice(key, 1);
+      for (const key in allRecords.value) {
+        if (allRecords.value[key]._id === id) {
+          allRecords.value.splice(key, 1);
         }
       }
 
-      fetchPage(currentPage.value);
+      fetchPage(allCurrentPage.value);
     };
 
     return {
@@ -207,6 +282,10 @@ export default {
       formHeaderFields,
       editRecordUrl,
       deleteRecord,
+      searchMode,
+      searchString,
+      searchStart,
+      searchStop,
     };
   },
 };
--- src/views/List.vue
+++ src/views/List.vue
@@ -1,17 +1,3 @@
-<i18n>
-  {
-    "en-US": {
-      "表單": "Form",
-      "結束": "Back",
-      "搜尋": "Search"
-    },
-    "zh-TW": {
-      "表單": "表單",
-      "結束": "結束",
-      "搜尋": "搜尋"
-    }
-  }
-</i18n>
 <template lang="pug">
 .list
   b-row
@@ -19,71 +5,27 @@
       b-input-group(size='sm')
         b-input-group-prepend(is-text) {{ $t('表單') }}
         b-form-select(v-model="form", :options="formNames" @change="onFormChange")
-    b-col(md='6')
-      b-input-group(size='sm')
-        b-input-group-prepend
-          b-button.mr-1(v-show="searchMode", @click="searchStop()") {{ $t('結束') }}
-          b-button(@click="searchStart()") {{ $t('搜尋') }}
-        b-form-input(type="text", v-model="searchString")
-  FormRecords#page-list(v-show="!searchMode", :form="form", button-edit-record, button-delete-record)
-  FormSearch#page-list(v-show="searchMode", :form="searchForm", :search="search", button-edit-record, button-delete-record)
-  //| {{records}}
+  FormRecords#page-list(:form="form", button-edit-record, button-delete-record)
 </template>
 <script>
-//import { useStore } from "vuex";
 import { ref, watch } from "@vue/composition-api";
 import FormRecords from "@/components/FormRecords.vue"; // @ is an alias to /src
-import FormSearch from "@/components/FormSearch.vue"; // @ is an alias to /src
 import * as gql from "@/components/lib/js/GraphQL.js";
 
 export default {
   props: {},
   components: {
     FormRecords,
-    FormSearch,
   },
   setup(props, context) {
-    //const _store = context.root.$store;
-    //console.log(context, _store.state);
-    //const _store = useStore();
     const _id = "List__";
 
     const forms = ref([]);
     const formNames = ref([]);
-    //const form = ref(_store.state.form2);
     const form = ref("");
     if (localStorage.getItem(_id + "form")) {
       form.value = localStorage.getItem(_id + "form");
     }
-
-    const searchForm = ref("");
-    const searchMode = ref(false);
-    if (localStorage.getItem(_id + "searchMode")) {
-      searchMode.value = localStorage.getItem(_id + "searchMode") == "true";
-    }
-    const searchString = ref("");
-    const search = ref("");
-    if (localStorage.getItem(_id + "search")) {
-      searchString.value = localStorage.getItem(_id + "search");
-    }
-
-    if (searchMode.value && form.value != "" && searchString.value != "") {
-      searchForm.value = form.value;
-      search.value = searchString.value;
-    }
-
-    const searchStart = () => {
-      searchMode.value = true;
-      localStorage.setItem(_id + "searchMode", searchMode.value);
-      search.value = "";
-      searchForm.value = form.value;
-      search.value = searchString.value;
-      localStorage.setItem(_id + "search", search.value);
-    };
-    const searchStop = () => {
-      searchMode.value = false;
-      localStorage.setItem(_id + "searchMode", searchMode.value);
-    };
 
     gql.getForms().then(
       (r) => {
@@ -104,12 +46,6 @@ export default {
       form,
       formNames,
       onFormChange,
-      searchForm,
-      searchMode,
-      searchString,
-      search,
-      searchStart,
-      searchStop,
     };
   },
 };
